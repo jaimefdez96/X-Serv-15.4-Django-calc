1d9149719976e2dbac86227fd902204a1dd9616c
Jaime Fernandez Sanchez <jfesa@alpha11.aulas.gsyc.urjc.es>
2018-04-03 17:44:38 +0200

    myviews

--- calc/views.py
+++ calc/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from . import calculadora
 # Create your views here.
+
+def barra(request):
+    return HttpResponse("Calculadora Online")
+
+def suma(request,op1,op2):
+    print(request)
+    num1 = int(op1)
+    num2 = int(op2)
+    resultado = calculadora.suma(num1,num2)
+    return HttpResponse("Resultado: " + str(resultado))
+
+def resta(request,op1,op2):
+    num1 = int(op1)
+    num2 = int(op2)
+    resultado = calculadora.resta(num1,num2)
+    return HttpResponse("Resultado: " + str(resultado))
+
+def multiplicacion(request,op1,op2):
+    num1 = int(op1)
+    num2 = int(op2)
+    resultado = calculadora.multiplicacion(num1,num2)
+    return HttpResponse("Resultado: " + str(resultado))
+
+def division(request,op1,op2):
+    num1 = int(op1)
+    num2 = int(op2)
+    resultado = calculadora.division(num1,num2)
+    return HttpResponse("Resultado: " + str(resultado))
+
+def error(request):
+    return HttpResponse("404 Not Found")
